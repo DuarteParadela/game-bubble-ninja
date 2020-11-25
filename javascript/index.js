f57021c9a1d403efa40b0a8806169e9c44c23849
Duarte <duarte.paradela@gmail.com>
2020-11-25 17:14:04 +0100

    Added the collision. Game over alert. Score incrementation. Bubble appearings over time

--- javascript/index.js
+++ javascript/index.js
@@ -1,6 +1,10 @@
 const canvas = document.querySelector("canvas");
 let WIDTH = 1280;
 let HEIGHT = 800;
+
+// let WIDTH = window.innerWidth;
+// let HEIGHT = window.innerHeight;
+
 // Règle la taille du canvas sur la taille de la fenêtre
 canvas.width = WIDTH;
 canvas.height = HEIGHT;
@@ -39,18 +43,71 @@ let state = {
 var img = new Image(); // Crée un nouvel élément Image
 img.src = "../images/IDLE-RIGHT.png";
 
+// class Chronometer {
+//   constructor() {
+//   this.currentTime = 0;
+//   this.intervalId = 0;
+//   }
+//   this.intervalId = setInterval(() => {
+//     this.currentTime++;
+//   }, 1000)
+
+//   getMinutes() {
+//     return Math.floor(this.currentTime / 60);
+//   };
+
+//   getSeconds() {
+//     return this.currentTime % 60;
+//   };
+
+//   twoDigitsNumber(time) {
+//     if (time < 10) {
+//       return '0'+time;
+//     }
+//     return time.toString();
+//   };
+
+//   resetClock() {
+//     this.currentTime = 0;
+//   };
+// }
+//   const chronometer = new Chronometer();
+
+//   function printMinutes() {
+//     const minutes = chronometer.getMinutes();
+//     return chronometer.twoDigitsNumber(minutes);
+//   }
+
+//   function printSeconds() {
+//     const seconds = chronometer.getSeconds();
+//     return chronometer.twoDigitsNumber(seconds);
+//   }
+
+//   function printTime() {
+
+//   }
+
+let counter = 0;
+setInterval(function () {
+  counter++;
+  document.getElementById("Score-point").textContent = counter;
+}, 200);
+
+setInterval(function () {
+  createNewCircle();
+}, 5000);
+
 class Player {
   constructor(x, y) {
     this.x = x;
     this.y = y;
     this.dx = 15;
-    this.size = 40;
+    this.radius = 40;
   }
   draw() {
     ctx.beginPath();
-    ctx.drawImage(img, this.x, this.y, this.size, this.size);
-    // ctx.fillRect(this.x, this.y, this.size, this.size);
-    ctx.strokeStyle = "blue";
+    ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+    ctx.strokeStyle = "red";
     ctx.stroke();
   }
 
@@ -62,11 +119,11 @@ class Player {
       this.x += this.dx;
     }
 
-    if (this.x + this.size > WIDTH) {
-      this.x = WIDTH - this.size;
+    if (this.x + this.radius > WIDTH) {
+      this.x = WIDTH - this.radius;
     }
-    if (this.x < 0) {
-      this.x = 0;
+    if (this.x < this.radius) {
+      this.x = this.radius;
     }
     this.draw();
   }
@@ -102,7 +159,7 @@ class Circle {
 
 let circleArray = [];
 
-for (let i = 0; i < 50; i++) {
+function createNewCircle() {
   let radius = 50;
   let x = Math.random() * (WIDTH - radius * 2) + radius;
   let y = Math.random() * (HEIGHT - radius * 2) + radius;
@@ -111,17 +168,49 @@ for (let i = 0; i < 50; i++) {
   circleArray.push(new Circle(x, y, dx, dy, radius));
 }
 
+// function init() {
+//   for (let i = 0; i < 5; i++) {
+//     let radius = 50;
+//     let x = Math.random() * (WIDTH - radius * 2) + radius;
+//     let y = Math.random() * (HEIGHT - radius * 2) + radius;
+//     let dx = 5;
+//     let dy = 5;
+//     circleArray.push(new Circle(x, y, dx, dy, radius));
+//   }
+// }
+
 let player = new Player(WIDTH / 2, HEIGHT - 40);
 
+function getDistance(xpos1, ypos1, xpos2, ypos2) {
+  let result = Math.sqrt(
+    Math.pow(xpos2 - xpos1, 2) + Math.pow(ypos2 - ypos1, 2)
+  );
+  return result;
+}
+
+function checkCollision(player, circle) {
+  let distance = getDistance(player.x, player.y, circle.x, circle.y);
+  if (distance < circle.radius + player.radius) {
+    alert(`GAME OVER.\nYou scored ${counter} points`);
+    cancelAnimationFrame(request);
+    document.location.reload();
+    // console.log("hey");
+  }
+}
+
+let request;
+
 function animate() {
-  requestAnimationFrame(animate);
+  request = requestAnimationFrame(animate);
   ctx.clearRect(0, 0, WIDTH, HEIGHT);
-  circleArray.forEach((elem) => {
-    elem.update();
+  circleArray.forEach((circle) => {
+    circle.update();
+    checkCollision(player, circle);
   });
   player.update();
 }
 
+// init();
 animate();
 
 function keyDown(event) {
